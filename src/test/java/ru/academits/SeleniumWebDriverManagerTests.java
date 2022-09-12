6484641b04d9c15a0780d5b2a426ccabb22f3f9a
uvolkova <uvolkova234@gmail.com>
2022-09-12 23:08:29 +0700

    Added variables, changed path, removed redudant assertions.

--- src/test/java/ru/academits/SeleniumWebDriverManagerTests.java
+++ src/test/java/ru/academits/SeleniumWebDriverManagerTests.java
@@ -16,11 +16,14 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.Keys.PAGE_DOWN;
 
 public class SeleniumWebDriverManagerTests {
 
@@ -66,20 +69,32 @@ public class SeleniumWebDriverManagerTests {
 
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
 
-        WebElement firstNameInput = driver.findElement(By.cssSelector("#firstName"));
-        firstNameInput.sendKeys("Yuliya");
+        String firstName = "Yuliya";
+        String lastName = "Volkova";
+        String name = firstName +" "+ lastName;
+        String email = "test@gmail.com";
+        String mobile = "1234567899";
+        String subjects = "Biology";
+        String currentAddress = "Novosibirsk";
+        String dateOfBirth = "17 Jun 2005";
+        String hobbies = "Sports";
+        String gender = "Female";
+        String picture = "School.PNG";
+        String state = "NCR";
+        String city = "Delhi";
+        String stateAndCity = state +" "+ city;
+        String month = "June";
+        String year = "2005";
 
-        Assertions.assertEquals("Yuliya", firstNameInput.getAttribute("value"));
+
+        WebElement firstNameInput = driver.findElement(By.cssSelector("#firstName"));
+        firstNameInput.sendKeys(firstName);
 
         WebElement lastNameInput = driver.findElement(By.cssSelector("#lastName"));
-        lastNameInput.sendKeys("Volkova");
-
-        Assertions.assertEquals("Volkova", lastNameInput.getAttribute("value"));
+        lastNameInput.sendKeys(lastName);
 
         WebElement emailInput = driver.findElement(By.cssSelector("#userEmail"));
-        emailInput.sendKeys("test@gmail.com");
-
-        Assertions.assertEquals("test@gmail.com", emailInput.getAttribute("value"));
+        emailInput.sendKeys(email);
 
         WebElement genderRadioButton = driver.findElement(By.xpath("//input[@id='gender-radio-2']/parent::div"));
         genderRadioButton.click();
@@ -87,134 +102,110 @@ public class SeleniumWebDriverManagerTests {
         Assertions.assertTrue(driver.findElement(By.xpath("//input[@id='gender-radio-2']/parent::div/input")).isSelected());
 
         WebElement mobileInput = driver.findElement(By.cssSelector("input#userNumber"));
-        mobileInput.sendKeys("1234567899");
-
-        Assertions.assertEquals("1234567899", mobileInput.getAttribute("value"));
+        mobileInput.sendKeys(mobile);
 
         WebElement dateOfBirthInput = driver.findElement(By.cssSelector("input#dateOfBirthInput"));
         dateOfBirthInput.click();
 
-        Assertions.assertTrue(dateOfBirthInput.isEnabled());
-
         WebElement selectMonthDropdown = driver.findElement(By.cssSelector("select.react-datepicker__month-select"));
         Select select = new Select(selectMonthDropdown);
-        select.selectByVisibleText("June");
-        List<WebElement> allOptions = select.getOptions();
-
-        Assertions.assertEquals("June", allOptions.get(5).getText());
+        select.selectByVisibleText(month);
 
         WebElement selectYearDropdown = driver.findElement(By.cssSelector("select.react-datepicker__year-select"));
         Select select1 = new Select(selectYearDropdown);
-        select1.selectByVisibleText("2005");
-
-        Assertions.assertEquals("2005", selectYearDropdown.getAttribute("value"));
+        select1.selectByVisibleText(year);
 
         WebElement dayOfBirthInput = driver.findElement(By.xpath("//div[@aria-label='Choose Friday, June 17th, 2005']"));
         dayOfBirthInput.click();
         WebElement fullDateOfBirth = driver.findElement(By.cssSelector("input#dateOfBirthInput"));
 
-        Assertions.assertEquals("17 Jun 2005", fullDateOfBirth.getAttribute("value"));
+        Assertions.assertEquals(dateOfBirth, fullDateOfBirth.getAttribute("value"));
 
         WebElement subjectsInput = driver.findElement(By.cssSelector("input#subjectsInput"));
         subjectsInput.sendKeys("g");
 
-        Assertions.assertEquals("g", subjectsInput.getAttribute("value"));
-
         WebElement selectSubjects = driver.findElement(By.xpath("//div[contains(text(), 'Biology')]"));
         selectSubjects.click();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        WebElement selectSubjects1 = driver.findElement(By.xpath("//div[contains(text(), 'Biology')]"));
-
-        Assertions.assertEquals("Biology", selectSubjects1.getText());
 
         WebElement hobbiesCheckBox = driver.findElement(By.xpath("//*[@id='hobbiesWrapper']/div[2]/div[1]"));
         hobbiesCheckBox.click();
 
         Assertions.assertTrue(driver.findElement(By.xpath("//*[@id='hobbiesWrapper']/div[2]/div[1]/input")).isSelected());
 
-        WebElement chooseFileButton = driver.findElement(By.cssSelector("input#uploadPicture"));
-        Path path = Paths.get("C:/Users/Gleb/IdeaProjects/4-selenium WeDriverBase/School.PNG");
-
-        Assertions.assertTrue(Files.exists(path));
-
-        chooseFileButton.sendKeys("C:/Users/Gleb/IdeaProjects/4-selenium WeDriverBase/School.PNG");
-        Assertions.assertTrue(Files.exists(path));
+        File file = new File("src/School.PNG");
+        WebElement selectPictureButton = driver.findElement(By.id("uploadPicture"));
+        selectPictureButton.sendKeys(file.getAbsolutePath());
+        Assertions.assertTrue(Files.exists(Path.of("src/School.PNG")));
 
         WebElement currentAddressInput = driver.findElement(By.cssSelector("#currentAddress"));
-        currentAddressInput.sendKeys("Novosibirsk");
-
-        Assertions.assertEquals("Novosibirsk", currentAddressInput.getAttribute("value"));
-
+        currentAddressInput.sendKeys(currentAddress);
 
         WebElement stateInput = driver.findElement(By.cssSelector("input#react-select-3-input"));
         stateInput.sendKeys("n");
 
-        Assertions.assertEquals("n", stateInput.getAttribute("value"));
 
         WebElement stateSelect = driver.findElement(By.xpath("//div[contains(text(), 'NCR')]"));
         stateSelect.click();
-        WebElement stateSelect1 = driver.findElement(By.xpath("//div[contains(text(), 'NCR')]"));
-
-        Assertions.assertEquals("NCR", stateSelect1.getText());
 
         WebElement cityInput = driver.findElement(By.cssSelector("input#react-select-4-input"));
         cityInput.sendKeys("d");
 
-        Assertions.assertEquals("d", cityInput.getAttribute("value"));
-
         WebElement citySelect = driver.findElement(By.xpath("//*[contains(text(), 'Delhi')]"));
         citySelect.click();
-        WebElement citySelect1 = driver.findElement(By.xpath("//*[contains(text(), 'Delhi')]"));
-
-        Assertions.assertEquals("Delhi", citySelect1.getText());
 
         WebElement bookStoreDropdown = driver.findElement(By.xpath("//div[contains(text(), 'Book Store Application')]"));
         bookStoreDropdown.click();
 
+        WebDriverWait wait = new WebDriverWait(driver, 30, 100);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("submit")));
+
         Actions action = new Actions(driver);
         action.sendKeys(Keys.PAGE_DOWN).build().perform();
 
-        WebDriverWait wait = new WebDriverWait(driver, 30, 500);
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#submit")));
+        WebDriverWait wait2 = new WebDriverWait(driver, 30, 100);
+        wait2.until(ExpectedConditions.elementToBeClickable(By.id("submit")));
 
         WebElement submitButton = driver.findElement(By.cssSelector("#submit"));
         submitButton.click();
 
         WebElement nameValue = driver.findElement(By.xpath("//td[contains (text(), 'Yuliya Volkova')]"));
-        Assertions.assertEquals("Yuliya Volkova", nameValue.getText());
+        Assertions.assertEquals(name, nameValue.getText());
 
         WebElement nameStudentValue = driver.findElement(By.xpath("//td[contains (text(), 'test@gmail.com')]"));
-        Assertions.assertEquals("test@gmail.com", nameStudentValue.getText());
+        Assertions.assertEquals(email, nameStudentValue.getText());
 
         WebElement genderValue = driver.findElement(By.xpath("//td[contains (text(), 'Female')]"));
-        Assertions.assertEquals("Female", genderValue.getText());
+        Assertions.assertEquals(gender, genderValue.getText());
 
         WebElement mobileValue = driver.findElement(By.xpath("//td[contains (text(), '1234567899')]"));
-        Assertions.assertEquals("1234567899", mobileValue.getText());
+        Assertions.assertEquals(mobile, mobileValue.getText());
 
         WebElement dateOfBirthValue = driver.findElement(By.xpath("//td[contains (text(), '17 June,2005')]"));
         Assertions.assertEquals("17 June,2005", dateOfBirthValue.getText());
 
         WebElement subjectsValue = driver.findElement(By.xpath("//td[contains (text(), 'Biology')]"));
-        Assertions.assertEquals("Biology", subjectsValue.getText());
+        Assertions.assertEquals(subjects, subjectsValue.getText());
 
         WebElement hobbiesValue = driver.findElement(By.xpath("//td[contains (text(), 'Sports')]"));
-        Assertions.assertEquals("Sports", hobbiesValue.getText());
+        Assertions.assertEquals(hobbies, hobbiesValue.getText());
 
         WebElement pictureValue = driver.findElement(By.xpath("//td[contains (text(), 'School.PNG')]"));
-        Assertions.assertEquals("School.PNG", pictureValue.getText());
+        Assertions.assertEquals(picture, pictureValue.getText());
 
         WebElement addressValue = driver.findElement(By.xpath("//td[contains (text(), 'Novosibirsk')]"));
-        Assertions.assertEquals("Novosibirsk", addressValue.getText());
+        Assertions.assertEquals(currentAddress, addressValue.getText());
 
         WebElement stateAndCityValue = driver.findElement(By.xpath("//td[contains (text(), 'NCR Delhi')]"));
-        Assertions.assertEquals("NCR Delhi", stateAndCityValue.getText());
+        Assertions.assertEquals(stateAndCity, stateAndCityValue.getText());
+
+        WebDriverWait wait3 = new WebDriverWait(driver, 30, 500);
+        wait3.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#closeLargeModal")));
 
         Actions action1 = new Actions(driver);
         action1.sendKeys(Keys.PAGE_DOWN).build().perform();
 
         WebDriverWait wait1 = new WebDriverWait(driver, 30, 500);
-        wait1.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#submit")));
+        wait1.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#closeLargeModal")));
 
         WebElement closeButton = driver.findElement(By.cssSelector("#closeLargeModal"));
         closeButton.click();
